46073bfff713ba5e0380f792d8941001d9564e78
kirillkruglikov <kirillkruglikov@gmail.com>
2026-03-18 16:14:21 +0200

    add research_output and swarm_output to state

--- src/langchain_examples/agents/state.py
+++ src/langchain_examples/agents/state.py
@@ -15,3 +15,5 @@ class PipelineState(TypedDict):
     last_agent: str  # Track which agent is waiting for tool results
     editor_iteration: int  # How many times editor has reviewed (max 4 in conditional graph)
     factchecker_iteration: int  # How many times factchecker has reviewed (max 3 in conditional graph)
+    research_output: str
+    swarm_output: str
